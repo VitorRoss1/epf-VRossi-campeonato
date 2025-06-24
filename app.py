52e7b308329b2ec70dc4cd8534dc0b8d4764377e
Vitor Rossi <vitorcrossi159@gmail.com>
2025-06-24 11:21:24 -0300

    ajustes app py/campeonato cntrller

--- app.py
+++ app.py
@@ -7,21 +7,34 @@ class App:
         self.config = Config()
 
     def setup_routes(self):
-        # Importe explicitamente cada controller
-        from controllers.campeonato_controller import campeonato_routes
-        from controllers.user_controller import user_routes
+        # Importe os controllers
+        from controllers import campeonato_controller, user_controller
         
-        # Monte as rotas com prefixos
-        self.bottle.mount('/campeonato', campeonato_routes)
-        self.bottle.mount('/users', user_routes)
+        # Monte as rotas
+        self.bottle.mount('/campeonato', campeonato_controller.create_campeonato_routes())
+        self.bottle.mount('/users', user_controller.user_routes)
         
-        # Rota raiz para teste
+        # Rota raiz
         @self.bottle.route('/')
         def home():
             return """
-            <h1>Sistema do Brasileirão</h1>
-            <a href='/campeonato/rodada/1'>Ver Rodada 1</a>
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Brasileirão 2025</title>
+            </head>
+            <body>
+                <h1>Bem-vindo ao Sistema do Brasileirão 2025</h1>
+                <ul>
+                    <li><a href='/campeonato/rodada/1'>Simular Rodada</a></li>
+                    <li><a href='/campeonato/time/1'>Ver Time</a></li>
+                    <li><a href='/users'>Gerenciar Usuários</a></li>
+                </ul>
+            </body>
+            </html>
             """
+
+        print('✅ Rotas configuradas com sucesso!')
 
     def run(self):
         self.setup_routes()
--- controllers/campeonato_controller.py
+++ controllers/campeonato_controller.py
@@ -1,4 +1,4 @@
-from bottle import template, request 
+from bottle import template, request
 from .base_controller import BaseController
 from models.time import Time
 from models.partida import Partida
@@ -10,17 +10,20 @@ class CampeonatoController(BaseController):
         self.setup_routes()
 
     def setup_routes(self):
-        self.app.route('/campeonato/rodada/<rodada_numero>', callback=self.display_rodada)
-        self.app.route('/campeonato/enviar-placares', method='POST', callback=self.enviar_placares)
-        self.app.route('/campeonato/time/<time_id>', callback=self.display_time)
+        """Configura todas as rotas do campeonato"""
+        self.app.route('/rodada/<rodada_numero>', callback=self.display_rodada)
+        self.app.route('/enviar-placares', method='POST', callback=self.enviar_placares)
+        self.app.route('/time/<time_id>', callback=self.display_time)
 
     def display_rodada(self, rodada_numero):
+        """Exibe os jogos de uma rodada específica"""
         partidas = load_partidas_darodada(int(rodada_numero))
-        return self.render('rodada_partidas', 
-                         partidas=partidas, 
+        return self.render('rodada_partidas',
+                         partidas=partidas,
                          rodada=rodada_numero)
 
     def enviar_placares(self):
+        """Processa os placares enviados pelo formulário"""
         rodada_numero = int(request.forms.get('rodada'))
         partidas = load_partidas_darodada(rodada_numero)
 
@@ -30,8 +33,20 @@ class CampeonatoController(BaseController):
             partida.definir_placar(casa_gols, fora_gols)
 
         times = load_times()
-        return self.render('tabela_classificacao', times=times)
+        return self.render('tabela_classificacao', 
+                         times=sorted(times, 
+                                    key=lambda t: (-t.stats['Pontos'], 
+                                                 -t.Saldo_Gols())))
 
     def display_time(self, time_id):
+        """Exibe o elenco de um time específico"""
         time = next(t for t in load_times() if t.id == int(time_id))
-        return self.render('elenco_time', time=time)
+        return self.render('elenco_time', 
+                         time=time,
+                         jogadores=time.getJogadores)
+
+def create_campeonato_routes():
+    from bottle import Bottle
+    routes = Bottle()
+    CampeonatoController(routes)
+    return routes
